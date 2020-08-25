24af3a0317af708527c3372f41b89f8bb47f8119
Julio Melo <julio.melo@imd.ufrn.br>
2020-08-25 13:44:58 -0300

    Criando um fork para adicionar suporte à C usando GCC Criando um novo conjunto de constantes

---
+++ src/config/DefaultLanguages.js
@@ -0,0 +1,31 @@
+/*object representation,
+ key : ["backend_name", "frontend_name"]
+ ADD NEW LANGUAGES HERE!!!
+ */
+const languageObject = {
+    JAVASCRIPT: {backendName : "javascript", niceName: "JavaScript"},
+    CPP : {backendName:"cpp" , niceName:"C++"},
+    C : {backendName:"c", niceName:"C"},
+    PYTHON : {backendName:"python", niceName:"Python"},
+};
+
+/*
+    defaultLanguages.list contains the list of languages in the string format
+    defaultLanguages.lang.backendName contains the name of a given lang for the backend format e.g: languages.CPP.backendName == "cpp"
+    defaultLanguages.lang.niceName contains the name of a given lang for the frontend, aka "nice name", format e.g: languages.CPP.niceName == "C++"
+*/
+const DefaultLanguages = {
+    list : [],
+    niceNames : []
+};
+
+//creating language names list
+Object.values(languageObject).map( l => DefaultLanguages.list.push(l.backendName) );
+
+//creating niceNames list
+Object.values(languageObject).map( l => DefaultLanguages.niceNames.push(l.niceName) );
+
+//adding the language object in the DefaultLanguages object
+Object.keys(languageObject).map( k => DefaultLanguages[k]=languageObject[k] );
+
+export default DefaultLanguages;
